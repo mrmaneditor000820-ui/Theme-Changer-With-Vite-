adf497c5da1676413128379abfa25d9ec0ecb2f7
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-05-26 21:23:23 +0500

    styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from './Themeprovider'
+import "./App.css"
 
 
 const App = () => {
@@ -9,13 +10,20 @@ const App = () => {
   
 
   return (
+    <div id='main'>
+      <div id='main2'>
+        <h5>NAVBAR</h5>
+      </div>
+    
     <div style={{
       backgroundColor:theme === "dark" ? "black" : "white",
       color: theme === "light" ? "black" : "white",
-      height:"100vh"
+      height:"780px",margin:"0px",padding:"0px",
+      border:"2px solid blue"
     }}>
     <h1>ABDUL ---------- REHMAN</h1>
     <button onClick={toggleTheme}>change theme{theme}</button>
+    </div>
     </div>
   
   )
